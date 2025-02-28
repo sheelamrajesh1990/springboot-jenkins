7d48cf1982b943d3e9c4b78c40d4fa20d9e1da87
sheelamrajesh1990 <sheelamrajeshkumar0409@gmail.com>
2025-02-28 13:59:21 +0530

    added sample code

---
+++ src/test/java/com/spring/jenkins/api/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.spring.jenkins.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executed");
+		assertEquals(true, true);
+	}
+
+}
